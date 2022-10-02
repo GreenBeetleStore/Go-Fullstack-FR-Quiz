848d71cdc86b61dbaf68492e77cc096560c166b0
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-02 20:39:31 +0200

    Gorilla.png : OK !!!

--- backend/app.js
+++ backend/app.js
@@ -34,15 +34,14 @@ app.post("/api/products", (req, res, next) => {
   const product = new Product({
     ...req.body,
   });
-  Product
-    .save()
+  product.save()
     .then((product) => res.status(201).json({ product }))
     .catch((error) => res.status(400).json({ error }));
 });
 
 app.get("/api/products/:id", (req, res, next) => {
   Product.findOne({ _id: req.params.id })
-    .then((product) => res.status(200).json(product))
+    .then((product) => res.status(200).json({ product }))
     .catch((error) => res.status(404).json({ error }));
 });
 
@@ -60,7 +59,7 @@ app.delete("/api/products/:id", (req, res, next) => {
 
 app.get("/api/products", (req, res, next) => {
   Product.find()
-    .then((products) => res.status(200).json(products))
+    .then((products) => res.status(200).json({ products }))
     .catch((error) => res.status(400).json({ error }));
 });
 
